026e00b24e9896194f2f8d9256b0eda0b38a394d
Granvanoeli <levent.jade@gmail.com>
2014-03-03 12:24:07 +0000

    Add page works

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -79,20 +79,30 @@ def category(request, category_name_url):
 
 
 def add_category(request):
+    # Get the context from the request.
     context = RequestContext(request)
 
+    # A HTTP POST?
     if request.method == 'POST':
         form = CategoryForm(request.POST)
 
+        # Have we been provided with a valid form?
         if form.is_valid():
+            # Save the new category to the database.
             form.save(commit=True)
 
+            # Now call the index() view.
+            # The user will be shown the homepage.
             return index(request)
         else:
-            pass
+            # The supplied form contained errors - just print them to the terminal.
+            print form.errors
     else:
+        # If the request was not a POST, display the form to enter details.
         form = CategoryForm()
 
+    # Bad form (or form details), no form supplied...
+    # Render the form with error messages (if any).
     return render_to_response('rango/add_category.html', {'form': form}, context)
 
 
@@ -100,7 +110,6 @@ def add_page(request, category_name_url):
     context = RequestContext(request)
 
     category_name = decode_url(category_name_url)
-
     if request.method == 'POST':
         form = PageForm(request.POST)
 
@@ -111,9 +120,13 @@ def add_page(request, category_name_url):
 
             # Retrieve the associated Category object so we can add it.
             # Wrap the code in a try block - check if the category actually exists!
-
-            cat = Category.objects.get(name=category_name)
-            page.category = cat
+            try:
+                cat = Category.objects.get(name=category_name)
+                page.category = cat
+            except Category.DoesNotExist:
+                # If we get here, the category does not exist.
+                # Go back and render the add category form as a way of saying the category does not exist.
+                return render_to_response('rango/add_category.html', {}, context)
 
             # Also, create a default value for the number of views.
             page.views = 0
@@ -128,6 +141,7 @@ def add_page(request, category_name_url):
     else:
         form = PageForm()
 
-    return render_to_response('rango/add_page.html',
-                              {'category_name_url': category_name_url, 'category_name': category_name, 'form': form},
-                              context)
+    return render_to_response( 'rango/add_page.html',
+            {'category_name_url': category_name_url,
+             'category_name': category_name, 'form': form},
+             context)
