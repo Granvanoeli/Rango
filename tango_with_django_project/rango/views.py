1c19db74b8b3fa55072259916c97db6dce6b8162
Granvanoeli <levent.jade@gmail.com>
2014-03-04 01:23:28 +0000

    added Login, logout and restricted access

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -253,6 +253,11 @@ def user_login(request):
 # Use the login_required() decorator to ensure only those logged in can access the view.
 
 @login_required
+def restricted(request):
+    return HttpResponse("Since you're logged in, you can see this text!")
+
+
+@login_required
 def user_logout(request):
     # Since we know the user is logged in, we can now just log them out.
     logout(request)
